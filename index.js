79d4cd3fe33b53ada5b0faad480c3add366eab63
Alex Guo <alexqguo@gmail.com>
2018-03-18 15:40:57 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const hue = require('node-hue-api');
+const creds = require('./credentials');
+const HueApi = hue.HueApi;
+const lightState = hue.lightState;
+
+const host = creds.host;
+const username = creds.macbookUsername;
+const api = new HueApi(host, username);
+
+// Get basic config
+// api.config((err, config) => {
+//     if (err) throw err;
+//     console.log(JSON.stringify(config));
+// });
+
+// Groups
+// "Living room" has the id of 1 and contains the three main living room lights
+
+// Default light state of 70% brightness
+const onLightState = lightState.create().on().bri(178);
+const offLightState = lightState.create().on(false);
+
+// Generic display results callback
+function displayResult(result) {
+    console.log(JSON.stringify(result, null, 2));
+}
+
+// Generic error callback
+function displayError(error) {
+    console.error(error);
+}
+
+api.setGroupLightState(1, onLightState)
+    .catch(displayError)
+    .done();
